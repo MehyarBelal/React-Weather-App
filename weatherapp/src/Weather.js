379d647ba1e05193564d438333177c99a52d18bc
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2021-04-17 05:53:39 +1000

    Update Weather.js

--- weatherapp/src/Weather.js
+++ weatherapp/src/Weather.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Weather extends Component {
   constructor(props) {
     super(props);
-    this.state = { city: "Perth", temp: "" };
+    this.state = { city: "paris", temp: "" };
   }
 
   componentDidMount() {
@@ -13,14 +13,34 @@ export class Weather extends Component {
       `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`
     )
       .then((res) => res.json())
-      .then((json) => this.setState({ name: json.name, temp: json.main.temp }));
+      .then((json) => this.setState({ city: json.name, temp: json.main.temp }));
   }
 
   render() {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.state.city);
+      console.log(this.state.temp);
+    };
     return (
       <div>
         <h1>{this.state.city}</h1>
         <h2>Temp: {this.state.temp}</h2>
+        <div>
+          <label>Enter City</label>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={(e) => {
+                this.setState({ city: e.target.value });
+              }}
+              type="text"
+              place="Enter City"
+            />
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
