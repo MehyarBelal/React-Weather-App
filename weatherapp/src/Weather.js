9634dd6e5db38a7d8e946e58409ccec1d70eed18
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2021-04-16 14:33:40 +1000

    api up and running

--- weatherapp/src/Weather.js
+++ weatherapp/src/Weather.js
@@ -3,18 +3,26 @@ import React, { Component } from "react";
 export class Weather extends Component {
   constructor(props) {
     super(props);
+    this.state = { city: "Melbourne", temp: "" };
+  }
 
-    setState({ city: "" });
+  componentDidMount() {
     const api_key = "47ef9ccd2698d5863051758df299be08";
+
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`
+    )
+      .then((res) => res.json())
+      .then((json) => this.setState({ name: json.name, temp: json.main.temp }));
   }
 
-  componentDidCatch() {
-    fetch("http://example.com/movies.json")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>{this.state.city}</h1>
+        <h2>Temp: {this.state.temp}</h2>
+      </div>
+    );
   }
 }
 
